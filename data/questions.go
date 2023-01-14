af291f251f26d7cd66607dc278a50588e40c5b19
Valeri <valericfbg@gmail.com>
2023-01-14 18:03:57 -0500

    refactoring deleted options code

--- data/questions.go
+++ data/questions.go
@@ -60,39 +60,14 @@ func (dal QuestionsDal) Update(questionId uint, question *models.QuestionUpdate)
 			return err
 		}
 
-		// Handle deleted options
-		var options []models.Option
-
-		if err := dal.db.Model(models.Option{}).Find(&options).Where("question_id = ?", questionId).Error; err != nil {
+		if err := dal.handleDeletedOptions(questionId, question.Options); err != nil {
 			return err
-		}
-
-		dbOptions := map[uint]bool{}
-
-		for _, option := range options {
-			dbOptions[option.ID] = true
-		}
-
-		userOptions := map[uint]bool{}
-
-		for _, option := range question.Options {
-			userOptions[option.ID] = true
-		}
-
-		// Which options are in DB but not in user options - delete them
-		for id := range dbOptions {
-			if _, existsInUserOpts := userOptions[id]; !existsInUserOpts {
-				if err := dal.db.Delete(&models.Option{}, id).Error; err != nil {
-					return err
-				}
-			}
 		}
 
 		// Handle existing and new options
 		for _, option := range question.Options {
 			dalOption := option.ToDal(questionId)
 
-			// TODO: Create a method for that
 			if dal.db.Model(models.Option{}).Where("id = ? AND question_id = ?", option.ID, questionId).Updates(&dalOption).RowsAffected == 0 {
 				dal.db.Create(&dalOption)
 			}
@@ -100,4 +75,35 @@ func (dal QuestionsDal) Update(questionId uint, question *models.QuestionUpdate)
 
 		return nil
 	})
+}
+
+func (dal *QuestionsDal) handleDeletedOptions(questionId uint, optionsUpsert []models.OptionUpsert) error {
+	var options []models.Option
+
+	if err := dal.db.Model(models.Option{}).Find(&options).Where("question_id = ?", questionId).Error; err != nil {
+		return err
+	}
+
+	dbOptions := map[uint]bool{}
+
+	for _, option := range options {
+		dbOptions[option.ID] = true
+	}
+
+	userOptions := map[uint]bool{}
+
+	for _, option := range optionsUpsert {
+		userOptions[option.ID] = true
+	}
+
+	// Which options are in DB but not in user options - delete them
+	for id := range dbOptions {
+		if _, existsInUserOpts := userOptions[id]; !existsInUserOpts {
+			if err := dal.db.Delete(&models.Option{}, id).Error; err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
